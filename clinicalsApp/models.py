34619c5e0179d4cbfbe38a959483e229b83f7aec
Anil Kumar Guntha <anil.guntha9@gmail.com>
2022-01-29 22:22:43 +0530

    Initial Commit

---
+++ clinicalsApp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Patient(models.Model):
+    lastName=models.CharField(max_length=20)
+    firstName=models.CharField(max_length=20)
+    age=models.IntegerField()
+
+class ClinicalData(models.Model):
+    COMPONENT_NAMES=[{'hw','Height/Weight'},{'bp','Blood Pressure'},{'heartrate','Heart Rate'}]
+    componentName=models.CharField(max_length=20,choices=COMPONENT_NAMES)
+    componentValue=models.CharField(max_length=20)
+    measureDateTime=models.DateTimeField(auto_now_add=True)
+    patient = models.ForeignKey(Patient,on_delete=models.CASCADE)
